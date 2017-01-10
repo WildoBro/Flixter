6f9ee18c3b7493bf7fec0ceda0580b07e08c54eb
Willie Broder <willie.broder@gmail.com>
2017-01-10 01:14:00 +0000

    course enrollment validation

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,19 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_user_to_be_registered, only: [:show]
+
   def show
   end
 
   private 
   helper_method :current_lesson
+
+  def require_user_to_be_registered
+    if !current_user.enrolled_in?(current_lesson.section.course) 
+      redirect_to course_path(current_lesson.section.course), alert: "You are not enrolled in this course"
+    end
+  end
+
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
