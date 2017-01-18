f38f31a4d8c688df8684732576922d8f5d84224b
Willie Broder <willie.broder@gmail.com>
2017-01-18 02:51:08 +0000

    row order functionality for sections completed

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
   
   def new
     @section = Section.new
@@ -11,6 +12,10 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+
+  end
+
   private
 
   def require_authorized_for_current_course
@@ -19,12 +24,30 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
+  def require_authorized_for_current_section
+    if current_section.user != current_user
+      render text: "Unautherized", status: :unauthorized
+  end
+
   helper_method :current_course
   def current_course
-    @current_course ||= Course.find(params[:course_id])
+    if params[:course_id]
+      @current_course ||= Course.find(params[:course_id])
+    else
+      current_section.course
+    end
+  end
+
+  helper_method
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
   end
 
   def section_params
     params.require(:section).permit(:title, :row_order_position)
   end
 end
+
+# added helper_method (current_section)
+# added the before_action (before_action :require_authorized_for_current_section, only: [:update])
+# added the [:new, :create] to line 3
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create] 
     end
+    resources :sections, only: [:update]
   end
 
   # Example of regular route:
